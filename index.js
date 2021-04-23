2201212a35907015734f60069a90f8854cad1c14
Ruslan Haziev <hazievaleteya@yandex.ru>
2021-04-23 19:11:46 +0500

    fix problemm with game

--- index.js
+++ index.js
@@ -5,20 +5,20 @@ const __dirname = path.resolve()
 const PORT = 3000
 const app = express()
 
-app.use(express.static(path.resolve(__dirname, 'static')))
 
-
-app.get('/game', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'src', 'index.html'))
-})
+app.use(express.static(path.resolve(__dirname, 'public')))
 
 app.get('/', (req, res) =>{
-    res.sendFile(path.resolve(__dirname, 'static', 'autorization.html'))
+    res.sendFile(path.resolve(__dirname, 'public/auth', 'autorization.html'))
 })
 
-// app.get('/registration', (req, res) =>{
-//     res.sendFile(path.resolve(__dirname, 'static', 'registration.html'))
-// })
+app.get('/game', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'public/game', 'game.html'))
+})
+
+app.get('/registration', (req, res) =>{
+    res.sendFile(path.resolve(__dirname, 'public/auth', 'registration.html'))
+})
 
 app.listen(PORT, () => {
     console.log("Server has been started on port 3000, ", __dirname)
